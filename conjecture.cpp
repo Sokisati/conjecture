358d100056a46b66812d3672013e95f3b7841a93
Sokisati <yagizalpersoy@gmail.com>
2023-04-03 21:26:36 +0300

    initial commit

---
+++ conjecture.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int x,peak;
+    cout<<"sayi gir"<<endl;
+    cin>>x;
+    peak = x;
+
+   while(x>1)
+   {
+
+       if(x%2==0)
+       {
+
+           x = x/2;
+           cout<<x<<endl;
+           if(x > peak)
+           {
+              peak = x;
+           }
+
+       }
+
+       else
+        {
+
+            x = 3*x + 1;
+             cout<<x<<endl;
+               if(x > peak)
+           {
+              peak = x;
+           }
+
+        }
+
+   }
+
+cout<<"zirve nokta: "<<peak<<endl;
+
+return 0;
+}
